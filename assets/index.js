7a82e3e96610cb1a0123ed0652c9643c5ebd4e13
santechx <santiagogf11@gmail.com>
2020-06-25 23:00:26 -0500

    service worker

--- assets/index.js
+++ assets/index.js
@@ -15,3 +15,9 @@ const muteButton = document.getElementById('muteButton');
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
 playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+        console.log(error.message);
+    });
+}
