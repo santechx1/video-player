eb5c7156662124bc7921494b7ac4dad484d708b8
santechx <santiagogf11@gmail.com>
2020-06-26 23:59:01 -0500

    typescript basic types

--- exercises/typescript/index.ts
+++ exercises/typescript/index.ts
@@ -6,3 +6,45 @@ function add(a: number, b: number) {
 
 const sum = add(2, 3);
 
+/**
+ * TypeScript types
+ */
+// Booleans
+let muted: boolean = true;
+muted = false;
+
+// Numbers
+let x: number = 42
+let y: number = 6
+let z: number = x / y;
+
+// Strings
+let myName: string = 'Santiago';
+let greet: string = `My name is ${myName}`;
+
+// Arrays
+let people: string[] = [];
+people = ['Michael', 'Juan', 'Cristina'];
+people.push('Gabriela');
+
+let stringsAndNumbers: Array<string | number> = [];
+stringsAndNumbers.push('Hi');
+stringsAndNumbers.push(1000);
+
+// Enums
+enum Color {
+    Red = 'Red',
+    Green = 'Green',
+    Blue = 'Blue'
+}
+
+let favoriteColor: Color = Color.Blue;
+console.log(`My favorite color is ${favoriteColor}`);
+
+// Any
+let idk: any = 'Joker';
+idk = { type: 'Wildcard' };
+
+// Objects
+let someObject: object = { type: 'Wildcard' };
+
